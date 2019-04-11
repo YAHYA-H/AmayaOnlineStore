e13364890200b9da1c62e64c593d90f7b19d69c3
YAHYA-H <husain.host@gmail.com>
2019-04-11 20:02:00 +0300

    build the catalog views

--- store/models.py
+++ store/models.py
@@ -1,21 +1,30 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
 
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'category'
-        verbose_name_plural ='categories'
+    def get_absolute_url(self):
+        return reverse('store:product_list_category', args=[
+            self.slug
+        ])  #convention to retrieve the URL for a given objec
 
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
         def __str__(self):
             return self.name
 
+
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE) # many-to one relationship
+    category = models.ForeignKey(
+        Category, related_name='products',
+        on_delete=models.CASCADE)  # many-to one relationship
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products./%Y/%m/%d', blank=True)
@@ -25,10 +34,14 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[
+            self.id, self.slug
+        ])  #convention to retrieve the URL for a given objec
 
     class Meta:
-        ordering = ('name',)
-        index_together = (('id',  'slug'),)
+        ordering = ('name', )
+        index_together = (('id', 'slug'), )
 
     def __str__(self):
         return self.name
