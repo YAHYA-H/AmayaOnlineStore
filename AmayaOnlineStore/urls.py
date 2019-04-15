70144ce757fe704092cfe622e906341f2fbf6f19
YAHYA-H <husain.host@gmail.com>
2019-04-15 15:39:43 +0300

    add funcitonality for creating customer order

--- AmayaOnlineStore/urls.py
+++ AmayaOnlineStore/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('store.urls', namespace='store')),
+    path('orders/', include('orders.urls', namespace='orders'))
 ]
 
 # development server to upload images files
