46037c4a5bcf4a7f9f289d11db1a0826557dd21b
YAHYA-H <husain.host@gmail.com>
2019-04-15 14:53:43 +0300

    create order models

--- AmayaOnlineStore/settings.py
+++ AmayaOnlineStore/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
     'cart.apps.CartConfig',
-    'orders.apps.OrderConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from store.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    # store order details and customer info
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=25)
+    city = models.CharField(max_length=80)
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created', )
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    # store the product, quantity and price paid for each items
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):  # returns the cost of the item
+        return self.price * self.quantity
