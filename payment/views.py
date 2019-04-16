adaa9247a538676611537a50888d59be5bbe90e2
YAHYA-H <husain.host@gmail.com>
2019-04-16 15:34:39 +0300

    sending pdf file by email

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,11 @@
 import braintree
 from orders.models import Order
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+from io import BytesIO
+import weasyprint
 
 
 def payment_process(request):
@@ -26,6 +31,24 @@ def payment_process(request):
             # store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+            # create invoice email
+            subject = 'AmayaOnlineStore - Invoice #{}'.format(order.id)
+            message = 'Please, find attached invoice for your recent purchase'
+            email = EmailMessage(subject, message, 'amaya@store.info',
+                                 [order.email])
+            # generate a PDF
+            html = render_to_string('orders/order/pdf.html', {'order': order})
+            out = BytesIO()
+            stylesheets = [
+                weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')
+            ]
+            weasyprint.HTML(string=html).write_pdf(out,
+                                                   stylesheets=stylesheets)
+            # attach pdf file
+            email.attach('order_{}.pdf'.format(order.id), out.getvalue(),
+                         'application/pdf')
+            # send email
+            email.send()
             return redirect('payment:done')
         else:
             return redirect('payment:cancelled')
