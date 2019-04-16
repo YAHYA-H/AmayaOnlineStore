ddce93de0c240442b4090fcf04dd964b3e92dcae
YAHYA-H <husain.host@gmail.com>
2019-04-16 12:01:27 +0300

    add exporting csv orders in the  Order admin site

--- orders/admin.py
+++ orders/admin.py
@@ -1,14 +1,49 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
 from django.contrib import admin
 from .models import Order, OrderItem
-
-# Register your models here.
 
 
 class OrderItemInline(admin.TabularInline):
     # an inline allows us to include a model on the same edit page its related model
     model = OrderItem
     raw_id_fields = ['product']
+
+
+# generic admin action that can be added to any ModelAdmin class.
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(
+        content_type='text/csv'
+    )  # tell browser that that the resp has to be treated as a CSV file
+    response['Content-Disposition'] = 'attachment;'\
+        'filename={}.csv'.format(opts.verbose_name
+        ) # Indicate that the HTTP response contains an attached file
+    writer = csv.writer(
+        response)  # create a csv obj that will write on the resp obj
+    fields = [
+        field for field in opts.get_fields(
+        )  # get the fields dynamically with `get_fields` of the model `-meta` options
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # write a first row with  header info -->(write header row including the fields names)
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:  #<- iterate over queryset  and write a row for each obj return by queryset
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime(
+                    '%d./%m/%Y'
+                )  # take care of formatting by datetime obj b/c the ouput value for csv has to be a str
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'  # customize the display name for the action in the template
 
 
 @admin.register(Order)
@@ -19,3 +54,4 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     last_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]  # Exporting orders to CSV files
